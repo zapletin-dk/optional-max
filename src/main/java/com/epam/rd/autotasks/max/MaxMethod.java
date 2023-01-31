7db37ad0c2444f42ade9c59b999729ae27d8abc0
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-31 03:12:48 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -3,7 +3,12 @@ package com.epam.rd.autotasks.max;
 import java.util.OptionalInt;
 
 public class MaxMethod {
-    public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+    public static OptionalInt max ( int[] values) {
+        if (values != null && values.length > 0) {
+            int max = values[0];
+            for (int i = 1; i < values.length; i++) {
+                if (values[i] > max) max = values[i];
+            } return OptionalInt.of(max);
+        } else return OptionalInt.empty();
     }
 }
